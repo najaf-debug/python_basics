725696af4e3995acdcd317ee339a39906fa26f21
najaf-debug <Najafalihaider68@gmail.com>
2026-01-01 21:22:07 +0500

    done some math operations

--- python-project/math_operations.py
+++ python-project/math_operations.py
@@ -1,3 +1,4 @@
+# Basic maths using python
 int_1 = 56
 int_2 = 12
 float_1 = 5.4
@@ -22,3 +23,24 @@ print("Subtraction float: ", float_2 - float_1)
 
 print("Division int: ", int_1 / int_2)
 print("Division float: ", float_2 / float_1)
+
+# Modulus operator in python gives the remainder when a number is divided by another number
+
+print(int_1%int_2)
+
+# floor division // this operator is used to divide two numbers and round down to nearest whole number
+
+print(int_1//int_2)
+
+# exponentiation operator **
+
+int_3 = 4
+int_4 = 2
+
+print(int_3**int_4)
+
+# round() function this is used to round a function to the nearest whole number
+float_2 = 12.9
+
+print(round(float_1))
+print(round(float_2))
